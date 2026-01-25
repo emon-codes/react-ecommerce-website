846c9904bb9b3cb2ef323ffca0b1a53ca1dfb967
EMON AHMED RANA <emon9996a@gmail.com>
2026-01-25 11:08:51 +0600

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Navbar from './components/Navbar'
 
 const App = () => {
   
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+             <Navbar/>
+      <Routes>
+
+        <Route path='/' element={<Home/>} ></Route>
+        <Route path='/products' element={<Products/>} ></Route>
+        <Route path='/contact' element={<Contact/>} ></Route>
+        <Route path='/about' element={<About/>} ></Route>
+        <Route path='/cart' element={<Cart/>} ></Route>
+      </Routes>
+    
+    </BrowserRouter>
     
   )
 }
